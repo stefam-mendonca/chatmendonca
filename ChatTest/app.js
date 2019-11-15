3bc67b3e280274026d8dcab34ee874bbe54fb2f2
Stefam Mendonça <smedonca@libertypowercorp.com>
2019-11-15 10:26:09 -0300

    test

---
+++ ChatTest/app.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+// passa o express para o http-server
+var http = require('http').Server(app);
+// passa o http-server par ao socketio
+var io = require('socket.io')(http);
+
+var count = 0;
+
+// cria uma rota para fornecer o arquivo index.html
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+// sempre que o socketio receber uma conexão vai devoltar realizar o broadcast dela
+io.on('connection', function (socket) {
+
+    console.log(socket.id);
+
+    console.log('connected');
+    count++;
+    console.log('Count: ' + count);
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function (msg) {
+        console.log(msg);
+        console.log('disconnected');
+        count--;
+        console.log('Count: ' + count);
+    });
+
+    
+});
+
+// inicia o servidor na porta informada, no caso vamo iniciar na porta 3000
+http.listen(3000, function () {
+    console.log('Servidor rodando em: http://localhost:3000');
+});
